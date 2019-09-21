1978db6586ccaa6268dd6e21acfefca5aefcbe51
jcardenas <josecardenas208@gmail.com>
2019-09-21 00:23:06 -0500

    component Badgelist
    add ccs badgelist

---
+++ src/components/BadgesList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './styles/BadgesList.css';
+
+
+class BadgesList extends React.Component {
+    render () {
+        return (
+                  
+                        <ul className = 'list-unstyled'>
+
+                         {this.props.badges.map((badges) => {
+                             return (
+                                
+                                 <li key = {badges.id} className = 'Badge__section-name-list'>
+                                     
+                                        <img className = 'Badge__avatar-list' src = {badges.avatarUrl} alt = "Logo list" />
+                                     <div>
+                                         <ul className = 'list-unstyled'>
+                      
+                                      <li>{badges.firstName} {badges.lastName}</li>  
+                                   
+                                     <li> {badges.jobTitle}</li>
+                                      
+                        
+                                      
+                                      <li>{badges.twitter}</li>
+                                      </ul>
+                                 
+                                 </div>
+                                 </li>
+                                 
+                                
+                             )
+                         })} 
+                          </ul>  
+                       
+        )
+    }
+}
+
+export default BadgesList;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BadgeNew from './pages/BadgeNew';
+//import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges';
 
 const container = document.getElementById('root');
 ReactDOM.render(
-    <BadgeNew />,
+    <Badges/>,
     container);
